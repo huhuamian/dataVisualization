1070dce41251b895e25b16f2633cdcae65996e13
善中   丘 <qiushanzhong@outlook.com>
2020-11-22 23:21:41 +0800

    创建数据可视化项目

---
+++ 第五部分_数据报表开发/datav-report-dev/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    hello world
+  </div>
+</template>
+
+<style lang="scss">
+html, body, #app {
+  width: 100%;
+  height: 100%;
+  margin:0;
+  padding: 0;
+
+}
+
+</style>
