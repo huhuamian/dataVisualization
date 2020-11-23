fe8ec0e9702c3082402d8e84db37f4e01cc196f5
善中   丘 <qiushanzhong@outlook.com>
2020-11-23 23:23:14 +0800

    top_view初步排版

--- 第五部分_数据报表开发/datav-report-dev/src/components/TopView/index.vue
+++ 第五部分_数据报表开发/datav-report-dev/src/components/TopView/index.vue
@@ -1,5 +1,28 @@
 <template>
-  <div class="top">top view</div>
+  <div class="top-view">
+    <el-row :gutter="20">
+      <el-col :span="6">
+        <el-card shadow="hover">
+          鼠标悬浮时显示
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card shadow="hover">
+          鼠标悬浮时显示
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card shadow="hover">
+          鼠标悬浮时显示
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card shadow="hover">
+          鼠标悬浮时显示
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
